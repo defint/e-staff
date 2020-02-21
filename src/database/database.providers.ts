08b8578bc084fb85c0411c3595a4e1be308e05db
Anton <anton.matrenin@introduct.tech>
2020-02-21 15:56:12 +0200

    Add office seed

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,19 +1,21 @@
 import { createConnection } from 'typeorm';
 import { DATABASE_CONNECTION } from '../constants';
 
+export const getConnection = async () =>
+         await createConnection({
+           type: 'postgres',
+           host: 'localhost',
+           port: 5432,
+           username: 'heroes_user',
+           password: 'password',
+           database: 'estaff',
+           entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+           synchronize: true,
+         });
+
 export const databaseProviders = [
   {
     provide: DATABASE_CONNECTION,
-    useFactory: async () =>
-      await createConnection({
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'heroes_user',
-        password: 'password',
-        database: 'estaff',
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
+    useFactory: getConnection,
   },
 ];
---
+++ src/seed.ts
@@ -0,0 +1,38 @@
+import * as yaml from 'js-yaml';
+import * as fs from 'fs';
+import { getConnection } from './database/database.providers';
+
+export async function loadFixtures(
+  name: string,
+): Promise<any> {
+  const dbConnection = await getConnection();
+
+  let items: any[] = [];
+  try {
+    const file: any = yaml.safeLoad(
+      fs.readFileSync(`./fixtures/${name}.yml`, 'utf8'),
+    );
+    console.log(file);
+    items = file['fixtures'];
+  } catch (e) {
+    console.log('fixtures error', e);
+  }
+
+  if (!items) {
+    return;
+  }
+
+  items.forEach(async (item: any) => {
+    console.log(item);
+    const entityName = Object.keys(item)[0];
+    const data = item[entityName];
+    await dbConnection
+      .createQueryBuilder()
+      .insert()
+      .into(entityName)
+      .values(data)
+      .execute();
+  });
+}
+
+loadFixtures("offices");
